3b84e8d4f6a7c2861199af9cad2a7c7ef931b37f
Suliaman Abass <suliaman.abass46@gmail.com>
2022-02-07 18:50:33 +0100

    Mode via Command Line

---
+++ README.md
@@ -0,0 +1,11 @@
+# AIEX4
+Solution for Exercise 4 of EARIN - Checkers game employing alpha-beta pruning
+# INSTALLATIONS
+```sh
+pip install pygame
+```
+# USAGE
+To Play as Black argument is 1, to Play as White - 2, and for AI vs AI - 3.
+```sh
+main.py 1
+```
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, math
+import pygame, math, sys
 from constants import SQUARE_SIZE, HEIGHT, WIDTH, WHITE,BLACK
 from checkers import Checkers
 from alpha_beta import  alpha_beta_mm_BLACK,alpha_beta_mm
@@ -19,6 +19,10 @@ def message(msg):
     pygame.display.update()
 
 
+print(sys.argv[1])
+mode = sys.argv[1]
+# 1 for WHITE , 2 For BLACK, 3 for AI vs AI
+
 pygame.init()
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Checkers')
@@ -28,68 +32,71 @@ run = True
 
 
 clock = pygame.time.Clock()
-while run:
-    clock.tick(FPS)
 
-    # AI is WHITE
-    if game.turn == WHITE:
+if mode == '1':
+    while run:
+        clock.tick(FPS)
+        # AI is WHITE
+        if game.turn == WHITE:
+            value, new_board = alpha_beta_mm(game.get_board(), max_depth, -math.inf, math.inf, WHITE)
+            game.turn_AI(new_board)
 
-        value, new_board = alpha_beta_mm(game.get_board(),max_depth,-math.inf,math.inf, WHITE)
-        game.turn_AI(new_board)
-
-    if game.winner() != None:
-        # print(game.winner())
-        message(game.winner() + ' WINS')
-        pygame.time.delay(5000)
-        run = False
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if game.winner() != None:
+            message(game.winner() + ' WINS')
+            pygame.time.delay(5000)
             run = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
-            row, col = get_mouse_position(pos)
-            game.select(row, col)
 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                row, col = get_mouse_position(pos)
+                game.select(row, col)
+        game.update()
+    pygame.quit()
 
+if mode =='2':
+    while run:
+        clock.tick(FPS)
+        # AI IS BLACK
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
 
-    # AI IS BLACK
-    # for event in pygame.event.get():
-    #     if event.type == pygame.QUIT:
-    #         run = False
-    #
-    #     if event.type == pygame.MOUSEBUTTONDOWN:
-    #         pos = pygame.mouse.get_pos()
-    #         row, col = get_mouse_position(pos)
-    #         game.select(row, col)
-    #     #FOR WINNING BY ELIMINATION
-    # if game.winner() != None:
-    #     print(game.winner())
-    #     run = False
-    #     #FOR WINNING BY BLOCKADE
-    # if game.Winner != None:
-    #     print(game.Winner)
-    #     run = False
-    #
-    # if game.turn == BLACK:
-    #     value, new_board = alpha_beta_mm_BLACK(game.get_board(), max_depth,-math.inf,math.inf, BLACK)
-    #
-    #     game.turn_AI(new_board)
-    #
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                row, col = get_mouse_position(pos)
+                game.select(row, col)
 
+        if game.winner() != None:
+            # print(game.winner())
+            message(game.winner() + ' WINS')
+            pygame.time.delay(5000)
+            run = False
 
-    # # AI vs AI
-    # if game.turn == WHITE:
-    #     value, new_board = alpha_beta_mm(game.get_board(), max_depth,-math.inf,math.inf, WHITE)
-    #     game.turn_AI(new_board)
-    #
-    # if game.winner() != None:
-    #     print(game.winner())
-    #     run = False
-    # pygame.time.delay(500)
-    # if game.turn == BLACK:
-    #     value, new_board = alpha_beta_mm_BLACK(game.get_board(), max_depth, -math.inf, math.inf, BLACK)
-    #     game.turn_AI(new_board)
+        if game.turn == BLACK:
+            value, new_board = alpha_beta_mm_BLACK(game.get_board(), max_depth,-math.inf,math.inf, BLACK)
+            game.turn_AI(new_board)
+        game.update()
+    pygame.quit()
 
-    game.update()
-pygame.quit()
+if mode == '3':
+    while run:
+        clock.tick(FPS)
+        # AI vs AI
+        if game.turn == WHITE:
+            value, new_board = alpha_beta_mm(game.get_board(), max_depth, -math.inf, math.inf, WHITE)
+            game.turn_AI(new_board)
+
+        if game.winner() != None:
+            print(game.winner())
+            run = False
+        pygame.time.delay(500)
+        if game.turn == BLACK:
+            value, new_board = alpha_beta_mm_BLACK(game.get_board(), max_depth, -math.inf, math.inf, BLACK)
+            game.turn_AI(new_board)
+
+        game.update()
+    pygame.quit()
+
